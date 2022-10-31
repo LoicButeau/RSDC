a60a4a7958853f4b50d649524d032a11275901e0
LoicButeau <loic.buteau@hec.ca>
2022-10-31 10:59:38 -0400

    corrected divided by 0 error

--- hmmscratch.py
+++ hmmscratch.py
@@ -8,7 +8,7 @@ import scipy.optimize as spo
 delta1 =0.3
 delta2=0.2
 phi=0.5
-sig=1
+sig=0.3
 
 transmat= [[0.5,0.5],
            [0.5,0.5]]
@@ -41,7 +41,7 @@ def constraint2(params):
     return 1-params[6]-params[7]
 
 def constraint3(params):
-    return params[3]
+    return params[3]-0.00001
 
 def hamilton(params):
     p1filt = np.zeros(len(y))
@@ -51,18 +51,19 @@ def hamilton(params):
     p1filt[0] = ((1 - params[7]) / (2 - params[4] - params[7]))
     p2filt[0] = ((1 - params[4]) / (2 - params[4] - params[7]))
 
-    print(params)
-    #initialisation des params[4]filt
     for t in range(1,len(y)):
+
     
-    
+        #etape 1
         p1pred = params[4]*p1filt[t-1] + params[6]*p2filt[t-1]
         p2pred = params[5]*p1filt[t-1] + params[7]*p2filt[t-1]
     
-        f[t] = (1/(math.sqrt(2*np.pi*params[3])*math.exp(-(y[t]-params[0]-params[2]*y[t-1])**2/(2*params[3])))*p1pred + 1/(math.sqrt(2*np.pi*params[3])*math.exp(-(y[t]-params[1]-params[2]*y[t-1])**2/(2*params[3])))*p2pred)
-    
-        p1filt[t] = 1/(math.sqrt(2*np.pi*params[3])*math.exp(-(y[t]-params[0]-params[2]*y[t-1])**2/(2*params[3])))*p1pred / f[t]
-        p2filt[t] = 1/(math.sqrt(2*np.pi*params[3])*math.exp(-(y[t]-params[1]-params[2]*y[t-1])**2/(2*params[3])))*p2pred / f[t]
+        #etape 2
+        f[t] = 1/(math.sqrt(2*np.pi*params[3]))*math.exp(-1*(y[t]-params[0]-params[2]*y[t-1])**2/(2*params[3]))*p1pred + 1/(math.sqrt(2*np.pi*params[3]))*math.exp(-1*(y[t]-params[1]-params[2]*y[t-1])**2/(2*params[3]))*p2pred
+            
+        #etape 3
+        p1filt[t] = 1/(math.sqrt(2*np.pi*params[3]))*math.exp(-1*(y[t]-params[0]-params[2]*y[t-1])**2/(2*params[3]))*p1pred / f[t]
+        p2filt[t] = 1/(math.sqrt(2*np.pi*params[3]))*math.exp(-1*(y[t]-params[1]-params[2]*y[t-1])**2/(2*params[3]))*p2pred / f[t]
 
     log = np.sum(f)
     return -log
@@ -71,8 +72,8 @@ con1 = {"type": "eq", "fun": constraint1}
 con2 = {"type": "eq", "fun": constraint2}
 con3 = {"type": "ineq", "fun": constraint3}
 cons = [con1,con2,con3]
-bounds = ((None,None),(None,None),(None,None),(0.01,1),(0,1),(0,1),(0,1),(0,1))
-xo=[0.1,0.1,0.1,0.1,0.5,0.5,0.5,0.5]
+bounds = ((None,None),(None,None),(None,None),(0.0001,1),(0,1),(0,1),(0,1),(0,1))
+xo=[0.1,0.1,0.1,0.1,0.2,0.8,0.3,0.7]
 
 
 optparams = spo.minimize(hamilton, x0=xo,constraints=cons, bounds = bounds)
